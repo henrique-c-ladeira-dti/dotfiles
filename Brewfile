3ad2537c140cbbe2a494f8de9575bd3c1bec0857
Henrique <henrique.c.ladeira@gmail.com>
2021-07-16 22:56:18 -0300

    ♻️ refactor: add Brewfile

---
+++ Brewfile
@@ -0,0 +1,13 @@
+# Taps
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+
+# Brews (Packages)
+brew "bat"
+brew "nvm"
+
+# Casks (applications)
+cask "alfred"
+cask "brave-browser"
+cask "visual-studio-code"
--- setup/homebrew.zsh
+++ setup/homebrew.zsh
@@ -4,9 +4,4 @@ echo "\n <<< Homebrew Setup >>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo "\n <<< Installing VS Code >>>\n"
-brew install --cask visual-studio-code
-
-echo "\n <<< Installing bat >>>\n"
-brew install bat
-echo "...done 🪄"
+brew bundle --verbose
